177af5c8ec11b2333948089e80d82b5fd61caee0
raffaellara <rafa.fjb@gmail.com>
2020-02-27 16:28:28 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,41 @@
+$(function() {
+	//Toggle responsive button
+	$('.toggle').click(function(){
+		$('nav').slideToggle();
+	});
+	// Slider Home
+	$('.slider').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
+	        arrows: false
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]	  
+	});
+});
